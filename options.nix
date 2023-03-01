20a9dc7e2ac110b7bfb3fec224d3d2129418d741
niten <niten@fudo.org>
2023-03-01 13:00:17 -0800

    descrition -> description

--- options.nix
+++ options.nix
@@ -158,7 +158,7 @@ in {
 
       debug = mkOption {
         type = bool;
-        descrition = "Enable verbose debugging.";
+        description = "Enable verbose debugging.";
         default = false;
       };
 
