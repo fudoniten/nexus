f08cc5e252b58772c96f1d12d8cfcaaef88d254a
niten <niten@fudo.org>
2023-03-05 15:46:20 -0800

    Print the pgpass file

--- powerdns.nix
+++ powerdns.nix
@@ -204,6 +204,7 @@ in {
             in pkgs.writeShellScript "powerdns-initialize-db.sh" ''
               ${mkPgpassFile}
               export HOME=$RUNTIME_DIRECTORY
+              ls -l $HOME/.pgpass
               if [ "$( psql -tAc "SELECT to_regclass('public.domains')" )" ]; then
                 logger "database initialized, skipping"
               else
