df5c61a1a106a2fabec6e5738533a3f2039130d0
niten <niten@fudo.org>
2023-03-10 14:29:44 -0800

    toString for port

--- server.nix
+++ server.nix
@@ -18,7 +18,8 @@ in {
         enableACME = true;
         forceSSL = true;
 
-        locations."/".proxyPass = "http://127.0.0.1:${cfg.internal-port}";
+        locations."/".proxyPass =
+          "http://127.0.0.1:${toString cfg.internal-port}";
       });
     };
 
